08a40134d650a50fed5b2aebd1ce37cb488b4450
yugps <yugpratapsingh8@gmail.com>
2023-11-23 02:29:18 +0530

    updated files

--- src/exception.py
+++ src/exception.py
@@ -1,31 +1,32 @@
-import sys
+import sys # we are using sys library because it will tell us the filename , line no on the system where we are getting the error 
 from src.logger import logging
-
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-
-    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
-
-    return error_message
 
 class CustomException(Exception):
     
-    def __init__(self, error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    def __init__(self,error,sys):
+        super().__init__()
+        self.sys=sys
+        self.error=error
+        _,_,exc_tb = self.sys.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        error_message = f"Error occured in python script name {file_name} line number {exc_tb.tb_lineno} , error message {str(self.error)}"
+        self.error_message=error_message
+    
+    def __str__(self): # we use __str__ (string method) here because we need a class method that doesn't require to be called __str__ returns string
+        
+        return self.error_message
 
-    def __str__(self):
-        return self.error_message  
+     
+    
+
+# This is how we will do the logging and exception handline throughout projects as it is done below 
     
 
 if __name__=="__main__":
-    logging.info("Logging has started")
+    logging.info("Logging has started") # this will tell what actually is happening right now in the code , will get saved in the logs folder 
 
     try:
-        a=1/0
+        a=1/0 # here we will write all our code that we are working with 
     except Exception as e:
         logging.info('Division by zero') 
-        raise CustomException(e,sys)
+        raise CustomException(e,sys) # this we will use everywhere after the code , in case if code didn't work then error will be recorded in the logs file
